3a1a3d353fccc8b39c907516729506922d122fec
dannyXSC <874794016@qq.com>
2021-12-09 20:04:49 +0800

    Initial commit

---
+++ src/main/java/com/youpass/NewYoupassBackendApplication.java
@@ -0,0 +1,13 @@
+package com.youpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewYoupassBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewYoupassBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/youpass/NewYoupassBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewYoupassBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
